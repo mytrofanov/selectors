ae0a72adc640ef3cd4443bf171e5d1780a026b29
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-09 17:56:03 +0300

    запрашиваю юзеров из соцсети

--- src/messages/MessagesContainer.js
+++ src/messages/MessagesContainer.js
@@ -1,12 +1,19 @@
 import React from "react";
 import {SetMessagesActionCreator} from "../redux/MessageReducer";
 import {connect} from "react-redux";
-import {Messages} from "./Messages";
 import axios from "axios";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+
 
 class MessagesContainer extends React.Component {
+
+
     componentDidMount() {
-        axios.get("http://jsonplaceholder.typicode.com/posts")
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users?count=10&page=1")
             .then(response => {
                 this.props.setMessages(response.data);
             });
@@ -15,14 +22,85 @@ class MessagesContainer extends React.Component {
     render() {
         return <div>
 
-            {this.props.messages.map(u =>
-                <Messages userId={u.userId} id={u.id} title={u.title} body={u.body} key={u.id}/>)
-            }
+            <div className="container">
+                <div className="loading">
+                    <Box sx={{width: '100%'}}>
+                        <LinearProgress/>
+                    </Box>
+
+
+                </div>
+
+
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained">Следующая страница</Button>
+                    <Button variant="contained">Получить 100 постов</Button>
+
+
+                </Stack>
+                <div className="searchBlock">
+               <span>
+                Искать в  таблице:
+
+                <input type="text" id="searchField"
+                       placeholder="search"
+                    // value={inputTextValue}
+                       key="searchField"
+                    // onChange={(event => {
+                    //     setInputTextValue(event.target.value)
+                    //     filtered(event.target.value)
+                    // })}
+                />
+
+            </span>
+
+
+                </div>
+
+
+                <div>Отображено записей {this.props.messages.length} из   </div>
+                <b> Страница №   </b>
+
+                <div className="AllTables">
+                    <div className="SmallTable">
+
+                        <div className="messageTableBlock">
+
+
+                            <table>
+
+                                <tbody>
+                                <tr>
+                                    <th>№ п/п</th>
+                                    <th>user Id</th>
+                                    <th>Name</th>
+
+                                </tr>
+
+                                {this.props.messages.map((post, index) =>
+
+                                    <tr key={index}>
+
+                                        <td> {index}</td>
+                                        <td> {post.id}</td>
+                                        <td> {post.name}</td>
+
+                                    </tr>)
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
 
 
         </div>
-    }
 
+
+    }
 }
 
 let mapDispatchToProps = (dispatch) => {
@@ -40,3 +118,4 @@ let MapStateToProps = (state) => {
 };
 
 export default connect(MapStateToProps, mapDispatchToProps)(MessagesContainer);
+
